2f215828fd4af9504065aa4df633a967d97afbfa
Vorwes <thabitbustanji@hotmail.com>
2026-02-28 22:38:40 +0300

    Add prediction logic

--- serving/src/inference/engine.rs
+++ serving/src/inference/engine.rs
@@ -35,3 +35,18 @@ fn load_labels(model_path: &str) -> Result<ModelConfig> {
     let label_config: ModelConfig = serde_json::from_str(&config_path)?;
     Ok(label_config)
 }
+
+fn get_label(config: &ModelConfig, max_index: u32) -> Result<String> {
+    Ok(config
+        .id2label
+        .get(&max_index.to_string())
+        .unwrap_or(&"Unknown".to_string())
+        .clone())
+}
+
+fn predict(model: &VisionTransformer, img: Tensor, config: &ModelConfig) -> Result<String> {
+    let logits = model.forward(&img)?;
+
+    let max_index = logits.squeeze(0)?.argmax(0)?.to_scalar::<u32>()?;
+    Ok(get_label(&config, max_index)?)
+}
