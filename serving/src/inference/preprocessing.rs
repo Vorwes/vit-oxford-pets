17692d7fec22e4677310290e24575628312ed080
Vorwes <thabitbustanji@hotmail.com>
2026-03-07 17:15:08 +0300

    Add image resizing

--- serving/src/inference/preprocessing.rs
+++ serving/src/inference/preprocessing.rs
@@ -1,9 +1,13 @@
 use anyhow::Result;
-use image::ImageReader;
+use image::{ImageReader, imageops::FilterType};
 use std::io::Cursor;
 
 fn load_image(img: &[u8]) -> Result<image::DynamicImage> {
     Ok(ImageReader::new(Cursor::new(img))
         .with_guessed_format()?
         .decode()?)
+}
+
+fn img_resize(img: &image::DynamicImage) -> image::ImageBuffer<image::Rgb<u8>, Vec<u8>> {
+    img.resize_exact(224, 224, FilterType::Triangle).to_rgb8()
 }
