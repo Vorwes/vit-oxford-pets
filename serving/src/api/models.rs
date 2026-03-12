b185f23a7364928b5f2733ffc81146395441374e
Vorwes <thabitbustanji@hotmail.com>
2026-03-12 05:47:55 +0300

    Add AppState struct

--- serving/src/api/models.rs
+++ serving/src/api/models.rs
@@ -1,6 +1,14 @@
 use serde::Serialize;
+use std::sync::Arc;
+
+use crate::inference::engine::ImageClassifier;
 
 #[derive(Debug, Serialize)]
 pub struct Prediction {
     pub label: String,
+}
+
+#[derive(Debug, Clone)]
+pub struct AppState {
+    pub model: Arc<ImageClassifier>,
 }
