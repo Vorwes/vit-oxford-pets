fec1c6a860841731b46f62a7c42f9ed6dfe0844c
Vorwes <thabitbustanji@hotmail.com>
2026-03-14 21:27:13 +0300

    Implement API

--- serving/src/main.rs
+++ serving/src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+mod api;
+mod inference;
+
+use api::models::AppState;
+use axum::{Router, routing::post};
+use candle_core::Device;
+use inference::engine::ImageClassifier;
+use std::sync::Arc;
+use tokio::net::TcpListener;
+
+use api::routes::predict_image;
+
+#[tokio::main]
+async fn main() {
+    let model_path = "../models/vit-pets-final";
+    let device = Device::Cpu;
+
+    let model = Arc::new(ImageClassifier::new(model_path).expect("Failed to load model"));
+    let app_state = AppState { model, device };
+
+    let app = Router::new()
+        .route("/predict", post(predict_image))
+        .with_state(app_state);
+    let listener = TcpListener::bind("0.0.0.0:3000")
+        .await
+        .expect("Failed to bind to address");
+
+    axum::serve(listener, app).await.unwrap();
 }
